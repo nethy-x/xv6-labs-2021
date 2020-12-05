87542b8475d505de12a17e43e746aaebcdd2aa43
lapcakjuraj@gmail.com <deletesk123@gmail.com>
2020-12-05 16:43:03 +0100

    threads done

--- notxv6/ph.c
+++ notxv6/ph.c
@@ -17,6 +17,8 @@ struct entry *table[NBUCKET];
 int keys[NKEYS];
 int nthread = 1;
 
+pthread_mutex_t lock;
+
 double
 now()
 {
@@ -46,6 +48,7 @@ void put(int key, int value)
     if (e->key == key)
       break;
   }
+  pthread_mutex_lock(&lock);
   if(e){
     // update the existing key.
     e->value = value;
@@ -53,6 +56,7 @@ void put(int key, int value)
     // the new is new.
     insert(key, value, &table[i], table[i]);
   }
+    pthread_mutex_unlock(&lock);
 }
 
 static struct entry*
@@ -62,9 +66,11 @@ get(int key)
 
 
   struct entry *e = 0;
+  pthread_mutex_lock(&lock);
   for (e = table[i]; e != 0; e = e->next) {
     if (e->key == key) break;
   }
+  pthread_mutex_unlock(&lock);
 
   return e;
 }
@@ -109,6 +115,7 @@ main(int argc, char *argv[])
   }
   nthread = atoi(argv[1]);
   tha = malloc(sizeof(pthread_t) * nthread);
+  pthread_mutex_init(&lock, NULL);
   srandom(0);
   assert(NKEYS % nthread == 0);
   for (int i = 0; i < NKEYS; i++) {
